cc8b64c91394a85c2974304fe720ef32cf2333e1
hklee <hk@ccgolftour.co.kr>
2020-08-15 19:22:05 +0900

    api 연결 안함

--- resort/src/pages/Rooms.js
+++ resort/src/pages/Rooms.js
@@ -2,9 +2,12 @@ import React from 'react'
 import Hero from '../components/Hero';
 import Banner from '../components/Banner';
 import {Link} from 'react-router-dom';
+import RoomContainer from '../components/RoomContainer';
+
 const Rooms = () => {
     return (
-        <div>
+        <>
+            
             <Hero hero="roomsHero">
                 <Banner title="our rooms">
                     <Link to="/" className="btn-primary">
@@ -12,7 +15,9 @@ const Rooms = () => {
                     </Link>
                 </Banner>
             </Hero>
-        </div>
+            <RoomContainer />
+            
+        </>
     )
 }
 
